173a49944dfd648f5770954990d4bc64062bc8ae
Geng Yuchao <gengyuchao11@163.com>
2021-07-06 12:04:35 +0800

    Add i2c read and write

--- esp32c3/max30102/components/Wire/src/i2c_port.c
+++ esp32c3/max30102/components/Wire/src/i2c_port.c
@@ -18,6 +18,13 @@ extern "C" {
 #include "driver/i2c.h"
 #include "i2c_port.h"
 
+#define WRITE_BIT I2C_MASTER_WRITE              /*!< I2C master write */
+#define READ_BIT I2C_MASTER_READ  
+
+#define ACK_CHECK_EN 0x1                        /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS 0x0                       /*!< I2C master will not check ack from slave */
+#define ACK_VAL 0x0                             /*!< I2C ack value */
+#define NACK_VAL 0x1   
 
 #define I2C_MASTER_TX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
 #define I2C_MASTER_RX_BUF_DISABLE 0   
@@ -62,16 +69,52 @@ void i2cRelease(i2c_t i2c)
 {
     ;
 } // free ISR, Free DQ, Power off peripheral clock.  Must call i2cInit() to recover
-i2c_err_t i2cWrite(i2c_t i2c, uint16_t address, uint8_t* buff, uint16_t size, bool sendStop, uint16_t timeOutMillis)
+i2c_err_t i2cWrite(i2c_t i2c_num, uint16_t address, uint8_t* buff, uint16_t size, bool sendStop, uint16_t timeOutMillis)
 {
-    return I2C_ERROR_OK;
+    #if 0
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (address << 1) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write(cmd, buff, size, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, timeOutMillis);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+    #endif
+    if(sendStop == false) {
+        assert(0); //Don't sendStop not support now.
+    }
+    return i2c_master_write_to_device(i2c_num,address,buff,size,timeOutMillis);
 }
-i2c_err_t i2cRead(i2c_t i2c, uint16_t address, uint8_t* buff, uint16_t size, bool sendStop, uint16_t timeOutMillis, uint32_t *readCount)
+i2c_err_t i2cRead(i2c_t i2c_num, uint16_t address, uint8_t* buff, uint16_t size, bool sendStop, uint16_t timeOutMillis, uint32_t *readCount)
 {
-    return I2C_ERROR_OK;
+    #if 0
+    if (size == 0) {
+        return ESP_OK;
+    }
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (address << 1) | READ_BIT, ACK_CHECK_EN);
+    if (size > 1) {
+        i2c_master_read(cmd, buff, size - 1, ACK_VAL);
+    }
+    if(sendStop) {
+        i2c_master_read_byte(cmd, buff + size - 1, NACK_VAL);
+        i2c_master_stop(cmd);        
+    }
+
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, timeOutMillis);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+    #endif
+    if(sendStop == false) {
+        assert(0); //Don't sendStop not support now.
+    }
+    return i2c_master_read_from_device(i2c_num,address,buff,size,timeOutMillis);
 }
-i2c_err_t i2cFlush(i2c_t i2c)
+i2c_err_t i2cFlush(i2c_t i2c_num)
 {
+    // i2c_param_config(i2c_num, const i2c_config_t *i2c_conf);
     return I2C_ERROR_OK;
 }
 i2c_err_t i2cSetFrequency(i2c_t i2c, uint32_t clk_speed)
